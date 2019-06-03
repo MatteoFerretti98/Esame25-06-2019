ac07b9d96192e1bcc86b488bf520b1ee957478a0
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-03 16:22:57 +0200

    Creazione Progetto

---
+++ Esame25-06-2019/src/main/java/Progetto/Esame25062019Application.java
@@ -0,0 +1,13 @@
+package Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esame25062019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esame25062019Application.class, args);
+	}
+
+}
