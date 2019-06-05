31a1e40f2d997436b7dc531fe30b23faf36c49c2
AngeloDagostino <48442293+AngeloDagostino@users.noreply.github.com>
2019-06-05 10:28:38 +0200

    Creazione delle classi rappresentanti il dataset

---
+++ Esame25-06-2019/src/main/java/Progetto/Dati.java
@@ -0,0 +1,70 @@
+package Progetto;
+
+public class Dati {
+private int Fid;
+private String PreNorm;
+private String Comune;
+private String Provincia;
+private String Indirizzo;
+private long  Telefono;
+private long Fax;
+private String Tipo;
+private Punto Punto;
+
+//getters e setters
+
+public int getFid() {
+	return Fid;
+}
+public void setFid(int fid) {
+	Fid = fid;
+}
+public String getPreNorm() {
+	return PreNorm;
+}
+public void setPreNorm(String preNorm) {
+	PreNorm = preNorm;
+}
+public String getComune() {
+	return Comune;
+}
+public void setComune(String comune) {
+	Comune = comune;
+}
+public String getProvincia() {
+	return Provincia;
+}
+public void setProvincia(String provincia) {
+	Provincia = provincia;
+}
+public String getIndirizzo() {
+	return Indirizzo;
+}
+public void setIndirizzo(String indirizzo) {
+	Indirizzo = indirizzo;
+}
+public long getTelefono() {
+	return Telefono;
+}
+public void setTelefono(long telefono) {
+	Telefono = telefono;
+}
+public long getFax() {
+	return Fax;
+}
+public void setFax(long fax) {
+	Fax = fax;
+}
+public String getTipo() {
+	return Tipo;
+}
+public void setTipo(String tipo) {
+	Tipo = tipo;
+}
+public Punto getPunto() {
+	return Punto;
+}
+public void setPunto(Punto punto) {
+	Punto = punto;
+}
+}
---
+++ Esame25-06-2019/src/main/java/Progetto/Punto.java
@@ -0,0 +1,22 @@
+package Progetto;
+
+public class Punto {
+	
+	private float Latitudine;
+	private float Longitudine;
+	
+	//getters e setters
+	public float getLatitudine() {
+		return Latitudine;
+	}
+	public void setLatitudine(float latitudine) {
+		Latitudine = latitudine;
+	}
+	public float getLongitudine() {
+		return Longitudine;
+	}
+	public void setLongitudine(float longitudine) {
+		Longitudine = longitudine;
+	}
+	
+}
