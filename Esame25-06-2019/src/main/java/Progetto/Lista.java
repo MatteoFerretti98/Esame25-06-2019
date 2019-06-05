26a2afb58246af5bdaed6b60cee5b9908d54ef5d
AngeloDagostino <48442293+AngeloDagostino@users.noreply.github.com>
2019-06-05 11:26:14 +0200

    Modifica classe Lista

--- Esame25-06-2019/src/main/java/Progetto/Lista.java
+++ Esame25-06-2019/src/main/java/Progetto/Lista.java
@@ -1,7 +1,27 @@
 package Progetto;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+
 
 public class Lista {
 private List<Dati> lista = new ArrayList<>();
+
+//costruttore che inizializza la lista
+public Lista() {
+	String line = "";
+    String cvsSplitBy = ",";
+    try (BufferedReader br = new BufferedReader(new FileReader("APL-AgenziaPerIlLavoro.csv"))) {
+        while ((line = br.readLine()) != null) {
+            String[] spazio = line.split(cvsSplitBy);
+            lista.add(new Dati ());
+            
+            } } catch (IOException e) {
+	            e.printStackTrace();
+	            }
+ 
+    }
+}
 }
