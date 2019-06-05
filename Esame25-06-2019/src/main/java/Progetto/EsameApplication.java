832f131b05977cbcc22f9041300e5c2cf6043ceb
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-05 11:12:25 +0200

    Modifica Main

--- Esame25-06-2019/src/main/java/Progetto/EsameApplication.java
+++ Esame25-06-2019/src/main/java/Progetto/EsameApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Esame25062019Application {
+public class EsameApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Esame25062019Application.class, args);
+		SpringApplication.run(EsameApplication.class, args);
 	}
 
 }
