047ee5575c96e15af4f3812de50c537a9c6c98a6
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-06 18:58:11 +0200

    Creazione Classe MetaData

--- Esame25-06-2019/src/main/java/Progetto/Controller.java
+++ Esame25-06-2019/src/main/java/Progetto/Controller.java
@@ -1,13 +1,19 @@
 package Progetto;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@SuppressWarnings({ "unused" })
 @RestController
 public class Controller {
+	
 	@GetMapping("/data") //stampa il JSON del dataset
 	public Lista getDati() {
 		return new Lista();
 	}
+
 
 }
---
+++ Esame25-06-2019/src/main/java/Progetto/MetaData.java
@@ -0,0 +1,11 @@
+package Progetto;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+@SuppressWarnings({ "unused" })
+public class MetaData {
+	
+}
