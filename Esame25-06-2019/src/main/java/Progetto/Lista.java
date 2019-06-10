152beae112dc0e1a3c617fb16917c686d442a1f2
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-10 16:29:09 +0200

    https://github.com/MatteoFerretti98/Esame25-06-2019.git
    Conflicts:
    	Esame25-06-2019/src/main/java/Progetto/Lista.java

--- Esame25-06-2019/src/main/java/Progetto/Lista.java
+++ Esame25-06-2019/src/main/java/Progetto/Lista.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class Lista extends Container {
 //private List<Dati> lista = new ArrayList<>();
 
+	
 //costruttore che inizializza la lista
 public Lista() {
 	String line = "";
