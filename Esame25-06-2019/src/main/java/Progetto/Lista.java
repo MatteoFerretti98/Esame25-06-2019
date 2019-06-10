1cf5c481d95e4fd5113222d2a4f09d275de3e44c
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-10 16:06:19 +0200

    Modifiche Lista

--- Esame25-06-2019/src/main/java/Progetto/Lista.java
+++ Esame25-06-2019/src/main/java/Progetto/Lista.java
@@ -18,10 +18,9 @@ public Lista() throws FileNotFoundException, IOException{
     float latitudine=0;
     float longitudine =0 ;
     BufferedReader br = new BufferedReader(new FileReader("APL-AgenziaPerIlLavoro.csv"));
-       line = br.readLine(); //serve per saltare la prima riga del dataset che contiene i nomi dei campi
+    line = br.readLine(); //serve per saltare la prima riga del dataset che contiene i nomi dei campi
     	while ((line = br.readLine()) != null) {
-    		//System.out.println(line);
-    		
+
             String[] spazio = line.split(cvsSplitBy);
             if(spazio.length>=10) {//se il vettore è più lungo del numero dei campi non inizializza
             String[] LatLong;
@@ -31,66 +30,30 @@ public Lista() throws FileNotFoundException, IOException{
            	} 
            	else
            	{ latitudine=(float) (Float.parseFloat(spazio[8])/Math.pow(10, spazio[8].length() -2));}
+           	//spazio[8]=Float.toString(latitudine);
            	if(spazio[9].contains(",")) {
            	LatLong = spazio[9].split(LatLongSplit);
             longitudine = (float) (Float.parseFloat(LatLong[0])+(Float.parseFloat(LatLong[1])/Math.pow(10, LatLong[1].length())));
            	}else
            	{ longitudine=(float) (Float.parseFloat(spazio[9])/Math.pow(10, spazio[9].length() -2));}
+           	//spazio[9]=Float.toString(longitudine);
            	
            	
-           	
-            if (spazio[5].isEmpty())	{ spazio[5]="0";}
-            if ((spazio[5].contains(" "))||(spazio[5].contains("/"))||(spazio[5].contains("."))) {
-            	if(spazio[5].contains(".")) surplus = spazio[5].indexOf(".");
-            	if(spazio[5].contains("/")) surplus = spazio[5].indexOf("/");
-            	if(spazio[5].contains(" ")) surplus = spazio[5].indexOf(" ");
+            if (spazio[5].isEmpty())	{ spazio[5]="0";}	//se il numero di telefono è vuoto ci carica dentro 0 altrimenti da errore
+            if ((spazio[5].contains(" "))||(spazio[5].contains("/"))||(spazio[5].contains("."))) { //serve per non eseguire sempre i 3 if seguenti
+            	if(spazio[5].contains(".")) surplus = spazio[5].indexOf(".");	//se la stringa contiene il "." il valore in eccesso è quello
+            	if(spazio[5].contains("/")) surplus = spazio[5].indexOf("/");	//se la stringa contiene il "/" il valore in eccesso è quello
+            	if(spazio[5].contains(" ")) surplus = spazio[5].indexOf(" ");	//se la stringa contiene il " " il valore in eccesso è quello
     		    spazio[5] = (spazio[5].substring(0, surplus) + spazio[5].substring(surplus + 1));
             }
-            /*if ((spazio[5].contains(" "))||(spazio[5].contains("/"))||(spazio[5].contains("."))) //corregge eventuali spazi nel numero 
-            {
-            	if(spazio[5].contains(".")) {FaxSplit=".";}
-            	if(spazio[5].contains("/")) {FaxSplit="/";}
-            	if(spazio[5].contains(" ")) {FaxSplit=" ";}
-            	//System.out.println(spazio[5]);
-            	String[] telefono= spazio[5].split(FaxSplit);
-            	
-            	if (telefono.length==1) {		
-            	 spazio[5]=telefono[0];		    //Serve per controllare se telefono 
-            									//è stato splittato in 2 campi pieni o se
-            	}								//uno dei 2 è vuoto
-            	else if( telefono.length==2)
-            	{
-            		spazio[5]=telefono[0].concat(telefono[1]);
-            	}
-            	else {
-            		spazio[5]= charRemoveAt(spazio[5], 3);
-            }
-            }*/
-            if (spazio[6].isEmpty())	{ spazio[6]="0";}
-            if ((spazio[6].contains(" "))||(spazio[6].contains("/"))||(spazio[6].contains("."))) {
-            	if(spazio[6].contains(".")) surplus = spazio[6].indexOf(".");
-            	if(spazio[6].contains("/")) surplus = spazio[6].indexOf("/");
-            	if(spazio[6].contains(" ")) surplus = spazio[6].indexOf(" ");
+           
+            if (spazio[6].isEmpty())	{ spazio[6]="0";} //se il numero del fax è vuoto ci carica dentro 0 altrimenti da errore
+            if ((spazio[6].contains(" "))||(spazio[6].contains("/"))||(spazio[6].contains("."))) { //serve per non eseguire sempre i 3 if seguenti
+            	if(spazio[6].contains(".")) surplus = spazio[6].indexOf(".");	//se la stringa contiene il "." il valore in eccesso è quello
+            	if(spazio[6].contains("/")) surplus = spazio[6].indexOf("/");	//se la stringa contiene il "/" il valore in eccesso è quello
+            	if(spazio[6].contains(" ")) surplus = spazio[6].indexOf(" ");	//se la stringa contiene il " " il valore in eccesso è quello
     		    spazio[6] = (spazio[6].substring(0, surplus) + spazio[6].substring(surplus + 1));
-            }
-            /*if (spazio[6].contains(" ")||spazio[6].contains("/")||spazio[6].contains(".")) //corregge eventuali spazi nel numero 
-            {
-            	if(spazio[6].contains(".")) {FaxSplit=".";}
-            	if(spazio[6].contains("/")) {FaxSplit="/";}
-            	if(spazio[6].contains(" ")) {FaxSplit=" ";}
-            	String[] fax= spazio[6].split(FaxSplit);
-            	
-            	if (fax.length==1) {		
-
-            		spazio[6]=fax[0];		//Serve per controllare se telefono 
-            									//è stato splittato in 2 campi pieni o se uno dei 2 è vuoto
-            	}			
-            	else if (fax.length==2){
-            		spazio[6]=fax[0].concat(fax[1]);
-            	}	
-            	else {spazio[6]=fax[0].concat(fax[1]).concat(fax[2]);
-            }
-            }*/ 
+            } 
             
             System.out.println(line);
             lista.add(new Dati (Integer.parseInt(spazio[0]),spazio[1],spazio[2],spazio[3],spazio[4],Long.parseLong(spazio[5]),Long.parseLong(spazio[6]),spazio[7],latitudine,longitudine));
