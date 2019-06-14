b858ac10deeb4d381d7bd32246540636220b0007
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-14 15:47:08 +0200

    Bug Fixes

--- Esame25-06-2019/src/main/java/Progetto/Controller.java
+++ Esame25-06-2019/src/main/java/Progetto/Controller.java
@@ -25,20 +25,7 @@ public class Controller {
 	public ResponseEntity GetMetadata() throws FileNotFoundException, IOException, ClassNotFoundException
 	{	
 		return new ResponseEntity <MetaData> (ListMeta,HttpStatus.NOT_FOUND); 
-		//return new MetaData();
 	}
-	
-	/*@GetMapping("/filter")
-    public ResponseEntity getbohh (@RequestParam String prov) throws JSONException{
-		filtrata.getList().clear();
-    	int size=Prima.getSize();
-    	for(int a=0; a<=size-1; a++)
-    	{
-    		if(Prima.containsProvincia(prov, a))	filtrata.getList().add(Prima.getDati(a));
-    	}
-		if (filtrata.isEmpty()) return new ResponseEntity <String>("Non esiste",HttpStatus.NOT_FOUND);
-		else return new ResponseEntity <Lista> (filtrata,HttpStatus.NOT_FOUND); 
-	}*/
 
 	@GetMapping("/filtr")
     public ResponseEntity filtro1 (@RequestParam String prov) throws JSONException
