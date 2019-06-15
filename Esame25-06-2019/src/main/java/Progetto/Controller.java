883714b972f0acf115ae409e0f7b27fd5f4d893a
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-06-16 01:00:40 +0200

    Miglioramento Controller

--- Esame25-06-2019/src/main/java/Progetto/Controller.java
+++ Esame25-06-2019/src/main/java/Progetto/Controller.java
@@ -30,32 +30,36 @@ public class Controller {
 		return new ResponseEntity <MetaData> (ListMeta,HttpStatus.NOT_FOUND); 
 	}
 
-	@GetMapping("/filtr")
+	/*@GetMapping("/filtr")
     public ResponseEntity filtro1 (@RequestParam String prov) throws JSONException
 	{
 		return new ResponseEntity (filtro.filterEq(prov, Prima, filtrata),HttpStatus.NOT_FOUND);
-	}
+	}*/
 	
 	@GetMapping("/filtro")
-	public ResponseEntity filtro2 (@RequestParam String tipo,String campo, String min, String max) throws JSONException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
+	public ResponseEntity filtro1 (@RequestParam String campo,String tipo, String min, String max) throws JSONException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
 	{
-		return new ResponseEntity (filtro.filterBT(tipo, campo, min, max, Prima, filtrata),HttpStatus.NOT_FOUND);
+		if((tipo!=null)&&(campo!=null)&&(min!=null)&&(max!=null))	return new ResponseEntity (filtro.filterBT(tipo, campo, min, max, Prima, filtrata),HttpStatus.NOT_FOUND);
+		else if((campo!=null)&&(tipo==null)&&(min==null)&&(max==null)) return new ResponseEntity (filtro.filterEq(campo, Prima, filtrata),HttpStatus.NOT_FOUND);
+		else return new ResponseEntity ("Immetti dei valori consoni",HttpStatus.NOT_FOUND);
 	}
 	
-	@GetMapping("/media")
+	/*@GetMapping("/media")
 	public String Media(@RequestParam String campo) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		float media = stats.Media(Prima, campo);
 		float devStand= stats.devStand(Prima, campo);
 		return "  del campo "+ campo +  " la media e': "+ media +" "+ "la deviazione standard è:" +devStand;
 		
-	}
+	}*/
 	
-	@GetMapping("/count")
-	public float Count(@RequestParam String campo, String nome) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		/*if((campo!=null)&&(nome!=null)) return stats.Count(Prima,campo, nome);
-		if((campo!=null)&&(nome==null))return 3;
-		else return 4;*/
-		return stats.Count(Prima,campo, nome);
+	@GetMapping("/stats")
+	public String Count(@RequestParam String campo, String nome) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		float media = stats.Media(Prima, campo);
+		float devStand= stats.devStand(Prima, campo);
+		float count= stats.Count(Prima, campo, nome);
+		if((campo!=null)&&(nome!=null)) return "Il conteggio del campo ''"+campo+"'' di ''"+nome+"'' è di: "+count;
+		else if((campo!=null)&&(nome==null)) return "  La media del campo ''"+ campo +  "'' e': "+ media + " e la deviazione standard è: " +devStand;
+		else return "Immettere i valori corretti";
 	}
 }
 
